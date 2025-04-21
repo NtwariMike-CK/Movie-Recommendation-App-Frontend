7007ff4d5cf271bf42c163b3ca5f1ea1d6933bd6
Ntwari Mike Chris Kevin <ntwarikevin95@gmail.com>
2025-04-21 13:30:16 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['image.tmdb.org'], // Add this line
+  },
 };
 
 export default nextConfig;
